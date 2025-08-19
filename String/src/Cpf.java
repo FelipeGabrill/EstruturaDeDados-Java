f2232999509db72a230a8ad51b29f96857257a40
Felipe Gabriel <felipegabrielodss@gmail.com>
2025-08-18 23:40:23 -0300

    cpf challenge

---
+++ String/src/Cpf.java
@@ -0,0 +1,39 @@
+
+public class Cpf {
+
+	public static void main(String[] args) {
+
+		System.out.println(removeNonDigits("949.237.847-99"));
+        System.out.println(removeNonDigits1("949.237.847-99"));
+        System.out.println(removeNonDigits2("949.237.847-99"));
+        
+	}
+
+	public static String removeNonDigits(String str) {
+		str = str.replace("-", "");
+		str = str.replace(".", "");
+
+		return str;
+	}
+
+	public static String removeNonDigits1(String str) {
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i))) {
+				sb.append(str.charAt(i));
+			}
+		}
+
+		return sb.toString();
+	}
+	
+	public static String removeNonDigits2(String str) {
+        String regex = "\\D";
+        return str.replaceAll(regex, "");
+    }
+	
+	
+
+}
