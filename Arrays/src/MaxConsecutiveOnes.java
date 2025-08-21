b88a8084ab23ab1323f432bb019c2968fb9d0351
Felipe Gabriel <felipegabrielodss@gmail.com>
2025-08-21 01:57:38 -0300

    Arrays: max consecutive ones challenge

---
+++ Arrays/src/MaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+
+public class MaxConsecutiveOnes {
+
+	public static void main(String[] args) {
+		
+		System.out.println(findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1,0,1,1,1,1}));  // 4
+        System.out.println(findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1}));  // 2
+	}
+
+	public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxOnes = 0;
+        int countOnes = 0;
+        
+        for (int value : nums) {
+            if (value == 1) {
+                countOnes++;
+            }
+
+            if (value != 1 && countOnes > 0) {
+                maxOnes = Math.max(maxOnes, countOnes);
+                countOnes = 0;
+            }
+        }
+
+        maxOnes = Math.max(maxOnes, countOnes);
+
+        return maxOnes;
+    }
+
+}
