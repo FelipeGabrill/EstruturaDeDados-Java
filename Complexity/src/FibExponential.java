f5e09d3838960f5d118bb6c7efe083d1cf4183bd
Felipe Gabriel <felipegabrielodss@gmail.com>
2025-09-02 02:53:19 -0300

    Complexity: FibExponential

---
+++ Complexity/src/FibExponential.java
@@ -0,0 +1,19 @@
+
+public class FibExponential {
+
+	public static void main(String[] args) {
+		int result = fibExponential(30);
+		System.out.println(result);
+	}
+
+	public static int fibExponential(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		return fibExponential(n - 1) + fibExponential(n - 2);
+	}
+
+}
